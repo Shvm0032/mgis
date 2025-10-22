85664d63f8d5d026465226d10e466570983760b3
Shvm0032 <shvmbutola@gmail.com>
2025-10-22 15:25:14 +0530

    ff

--- components/Loader.js
+++ components/Loader.js
@@ -12,8 +12,8 @@ export default function Loader() {
   if (!loading) return null;
 
   return (
-    <div className="fixed inset-0 z-[9999] h-[120vh] flex items-center justify-center bg-white overflow-hidden">
-      <div className="relative flex items-center justify-center w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-30 lg:h-30">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-white w-screen h-screen overflow-hidden">
+      <div className="relative flex items-center justify-center w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36">
         
         {/* Outer Spinner */}
         <div className="absolute w-full h-full rounded-full border-4 border-blue-300 border-t-[#00306E] animate-spin-slow"></div>
@@ -22,7 +22,7 @@ export default function Loader() {
         <img
           src="/Assets/logo/android.png"
           alt="Loader"
-          className="w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-20 lg:h-20 relative z-10"
+          className="w-14 h-14 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-24 lg:h-24 relative z-10"
         />
       </div>
     </div>
